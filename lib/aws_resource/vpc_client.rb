df152bc58cc774dee8b86348417e0e26388878d2
Daisuke Hirakiuchi <daisuke.hirakiuchi@leonisand.co>
2015-10-15 17:36:54 +0900

    add find_by_id,find_by_name to vpc_client

--- lib/aws_resource/vpc_client.rb
+++ lib/aws_resource/vpc_client.rb
@@ -18,5 +18,34 @@ module AwsResource
       vpcs
     end
     alias_method :vpcs, :each_vpcs
+
+    # Find vpc by vpc_id
+    #
+    # @param vpc_id [String] vpc_id
+    # @return AwsResource::Vpc instance or nil
+    def find_by_id(vpc_id)
+      vpcs(vpc_ids: [vpc_id]).first
+    rescue Aws::EC2::Errors::FilterLimitExceeded,
+           Aws::EC2::Errors::InvalidVpcIDNotFound => e
+      logger.warn e
+      nil
+    end
+
+    # Find vpc by name
+    #
+    # @param vpc_name [String] vpc name
+    # @return AwsResource::Vpc instance or nil
+    def find_by_name(vpc_name)
+      vpcs(
+        filters: [
+          { name: 'tag-key', values: ['Name'] },
+          { name: 'tag-value', values: [vpc_name] }
+        ]
+      ).first
+    rescue Aws::EC2::Errors::FilterLimitExceeded,
+           Aws::EC2::Errors::InvalidVpcIDNotFound => e
+      logger.warn e
+      nil
+    end
   end
 end
