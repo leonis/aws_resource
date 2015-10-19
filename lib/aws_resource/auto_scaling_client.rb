54fa37e9520cf69d868fd784acd55688375eeb38
Daisuke Hirakiuchi <daisuke.hirakiuchi@leonisand.co>
2015-10-19 16:24:55 +0900

    add launch_configurations related methods

--- lib/aws_resource/auto_scaling_client.rb
+++ lib/aws_resource/auto_scaling_client.rb
@@ -16,7 +16,7 @@ module AwsResource
         token = result.next_token
 
         result.auto_scaling_groups.each do |attrs|
-          as = AutoScalingGroup.new(attrs)
+          as = AutoScalingGroup.new(attrs, self)
           groups << as
           yield(as) if block_given?
         end
@@ -27,5 +27,47 @@ module AwsResource
       groups
     end
     alias_method :auto_scaling_groups, :each_auto_scaling_groups
+
+    def find_by_name(auto_scaling_group_name)
+      auto_scaling_groups(
+        auto_scaling_group_names: [auto_scaling_group_name]
+      ).first
+    rescue => e
+      logger.warn e
+      nil
+    end
+
+    def each_launch_configurations(options = {})
+      token = nil
+      configurations = []
+
+      loop do
+        result = @client.describe_launch_configurations(
+          options.merge(next_token: token)
+        )
+
+        token = result.next_token
+
+        result.launch_configurations.each do |attrs|
+          configuration = LaunchConfiguration.new(attrs)
+          configurations << configuration
+          yield(configuration) if block_given?
+        end
+
+        fail StopIteration if token.nil?
+      end
+
+      configurations
+    end
+    alias_method :launch_configurations, :each_launch_configurations
+
+    def find_launch_configuration_by_name(name)
+      launch_configurations(
+        launch_configuration_names: [name]
+      ).first
+    rescue => e
+      logger.warn e
+      nil
+    end
   end
 end
--- lib/aws_resource/auto_scaling_group.rb
+++ lib/aws_resource/auto_scaling_group.rb
@@ -1,4 +1,12 @@
 module AwsResource
   class AutoScalingGroup < Base
+    def launch_configuration
+      @client.find_launch_configuration_by_name(
+        launch_configuration_name
+      )
+    end
+  end
+
+  class LaunchConfiguration < Base
   end
 end
